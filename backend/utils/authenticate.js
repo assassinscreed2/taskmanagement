bc3b9447bbb24d69b1051c987dac923a35a33539
assassinscreed2 <ankursolanki1537@gmail.com>
2024-05-17 22:55:57 +0530

    Implement authorization middleware

---
+++ backend/utils/authenticate.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+
+const authenticate_request = (req, res, next) => {
+  const reqHead = req.headers["authorization"];
+  const token = reqHead.split(" ")[1];
+
+  if (token == null) {
+    return res.sendStatus(401);
+  }
+
+  jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+    if (err) {
+      return res.sendStatus(401);
+    }
+    req.user = user;
+    next();
+  });
+};
+
+module.exports = {
+  authenticate_request,
+};
