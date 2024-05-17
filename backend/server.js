e594a60f8f136fd3ada65de05ce5a36d4e7cd84f
assassinscreed2 <ankursolanki1537@gmail.com>
2024-05-17 13:40:16 +0530

    Initialize the backend server

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({ message: "API working successfully" });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Server started on PORT " + process.env.PORT);
+});
