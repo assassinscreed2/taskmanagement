cdbbd3ebebc2d5874a6398b425991b503f40cc53
assassinscreed2 <ankursolanki1537@gmail.com>
2024-05-19 16:01:38 +0530

    Create Task Schema and Model

--- backend/db/model.js
+++ backend/db/model.js
@@ -1,5 +1,6 @@
 const { Schema, model } = require("mongoose");
 
+// Profile Schema
 const profileSchema = Schema({
   firstname: {
     type: String,
@@ -18,8 +19,44 @@ const profileSchema = Schema({
   },
 });
 
+// Profile Model
 const profileModel = model("Profile", profileSchema);
+
+// Task Schema
+const taskSchema = Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  priority: {
+    type: Number,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+  },
+  dueDate: {
+    type: Date,
+    required: true,
+  },
+});
+
+// Task Model
+const taskModel = model("Task", taskSchema);
 
 module.exports = {
   profileModel,
+  taskModel,
 };
