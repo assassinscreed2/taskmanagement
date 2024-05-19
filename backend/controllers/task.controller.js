9d0afbcebfa8d63a67e165959b01c9c23d713017
assassinscreed2 <ankursolanki1537@gmail.com>
2024-05-19 16:36:29 +0530

    Create handler to update task

--- backend/controllers/task.controller.js
+++ backend/controllers/task.controller.js
@@ -44,6 +44,80 @@ async function createTask(req, res) {
   }
 }
 
+async function updateTask(req, res) {
+  try {
+    const { publish_id } = req.query;
+    const { title, description, priority, status, createdAt, dueDate } =
+      req.body;
+
+    // Check if atleast on field is provided
+    if (
+      !title &&
+      !description &&
+      !priority &&
+      !status &&
+      !createdAt &&
+      !dueDate
+    ) {
+      return res.status(400).json({
+        error: "At least one field is required",
+      });
+    }
+
+    const updates = {};
+    const allowedFields = [
+      "title",
+      "description",
+      "priority",
+      "status",
+      "createdAt",
+      "dueDate",
+    ];
+
+    // filter out fields which are not present
+    allowedFields.forEach((field) => {
+      if (req.body[field] !== undefined) {
+        updates[field] = req.body[field];
+      }
+    });
+
+    // get document ID from publish id
+    const internal_id = Buffer.from(publish_id, "base64").toString("utf8");
+
+    const updateFields = {
+      $set: updates,
+    };
+
+    const updatedTask = await Task.findByIdAndUpdate(
+      { _id: internal_id },
+      updateFields,
+      { new: true }
+    );
+
+    if (!updatedTask) {
+      return res.status(404).json({
+        error: "Task not found",
+      });
+    }
+
+    return res.status(200).json({
+      username: updatedTask.username,
+      title: updatedTask.title,
+      description: updatedTask.description,
+      priority: updatedTask.priority,
+      status: updatedTask.status,
+      createdAt: updatedTask.createdAt,
+      dueDate: updatedTask.dueDate,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      error: "Update task request failed",
+    });
+  }
+}
+
 module.exports = {
+  updateTask,
   createTask,
 };
