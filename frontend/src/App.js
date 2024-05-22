de38b1139b6d236a36bedfcf01c57a1b82d11a18
assassinscreed2 <ankursolanki1537@gmail.com>
2024-05-22 22:38:31 +0530

    Add Routes for components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,17 @@
-function App() {
-  return <div>Frontend</div>;
-}
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import TaskManager from "./components/TaskManager";
+import SignUp from "./components/SignUp";
+import SignIn from "./components/SignIn";
 
-export default App;
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<SignUp />} />
+        <Route path="/login" element={<SignIn />} />
+        <Route path="/tasks" element={<TaskManager />} />
+      </Routes>
+    </Router>
+  );
+}
